2f177f69002a7d11c87750b1d8f94ffb9a6a1a56
KKIvanov22 <KKIvanov22@codingburgas.bg>
2025-03-30 14:31:16 +0300

    Add example main menu

---
+++ app/linked-historians/menu.cpp
@@ -0,0 +1,33 @@
+#include "menu.h"
+
+void displayOptions()
+{
+	std::cout << "1. Add an event\n";
+	std::cout << "2. List all events\n";
+	std::cout << "9. Exit\n";
+}
+
+void displayMenu()
+{
+	char choice = ' ';
+
+	while (choice != '9')
+	{
+		displayOptions();
+		std::cout << "Enter your choice: ";
+		choice = _getch(); 
+		std::cout << choice << "\n"; 
+
+		switch (choice)
+		{
+		case '1':
+			break;
+		case '2':
+			break;
+		case '9':
+			break;
+		default:
+			break;
+		}
+	}
+}
---
+++ app/linked-historians/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <windows.h>
+#include <string>
+#define WIN32_LEAN_AND_MEAN
