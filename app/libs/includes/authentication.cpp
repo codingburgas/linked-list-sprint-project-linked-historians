32a79ad582b8fe071d254ab01453245895d7d6b1
ILLalev22 <132440245+ILLalev22@users.noreply.github.com>
2025-04-08 20:48:45 +0300

    Add QA Unit Test

--- app/libs/includes/authentication.cpp
+++ app/libs/includes/authentication.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include "authentication.h"
 #include "sha256.h"
 
--- app/linked-historians-QA/linked-historians-QA.cpp
+++ app/linked-historians-QA/linked-historians-QA.cpp
@@ -1,5 +1,6 @@
 #include "CppUnitTest.h"
 #include "utilities.h"
+#include "authentication.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -55,65 +56,86 @@ namespace linkedhistoriansQA
             Assert::IsFalse(isEmpty(input2));
             Assert::IsTrue(isEmpty(input3));
         }
-        TEST_METHOD(testEditEventData)
+
+        TEST_METHOD(testSearchEventByKeyword)
         {
             using namespace utilities;
 
-            std::string title = "Test_752";
-            std::string info = "Testing of the functionality.";
-            std::string type = "Battle";
-            std::string dateStr = "7/4/2025";
-
-            EVENT* head = new EVENT{ title, 0, 0, 0, info, nullptr, type };
-            formatDate(dateStr, head);
-
-            head->title = "Test_751";
-            head->info = "Editing an event test.";
-            head->type = "Treaty";
-            formatDate("1/1/2024", head);
-
-            Assert::AreEqual(std::string("Test_751"), head->title);
-            Assert::AreEqual(std::string("Editing an event test."), head->info);
-            Assert::AreEqual(std::string("Treaty"), head->type);
-            Assert::AreEqual(1, head->dateDay);
-            Assert::AreEqual(1, head->dateMonth);
-            Assert::AreEqual(2024, head->dateYear);
-
-            delete head;
-        }
-        TEST_METHOD(testDeleteEventFromList)
-        {
-            using namespace utilities;
-
-            EVENT* head = new EVENT{ "Test_752", 7, 4, 2025, "Deleting event test.", nullptr, "Battle" };
+            EVENT* head = new EVENT{ "Test_752", 7, 4, 2025, "Searching test.", nullptr, "Battle" };
             EVENT* second = new EVENT{ "Test_751", 1, 1, 2020, "Dummy event.", nullptr, "Treaty" };
             head->next = second;
 
-            std::string target = "Test_752";
+            std::string searchTitle = "Test_752";
+            std::string keyword = "test";
 
-            if (head != nullptr && head->title == target) {
-                EVENT* toDelete = head;
-                head = head->next;
-                delete toDelete;
-            }
-            else {
-                EVENT* prev = head;
-                EVENT* curr = head->next;
-                while (curr != nullptr && curr->title != target) {
-                    prev = curr;
-                    curr = curr->next;
+            EVENT* current = head;
+            bool foundTitle = false;
+            bool keywordFound = false;
+
+            while (current != nullptr) {
+                if (current->title == searchTitle) {
+                    foundTitle = true;
+                    if (containsString(current->info, keyword)) {
+                        keywordFound = true;
+                    }
+                    break;
                 }
-                if (curr != nullptr) {
-                    prev->next = curr->next;
-                    delete curr;
-                }
+                current = current->next;
             }
-            EVENT* check = head;
-            while (check != nullptr) {
-                Assert::AreNotEqual(std::string("Test_752"), check->title);
-                check = check->next;
-            }
+
+            Assert::IsTrue(foundTitle);
+            Assert::IsTrue(keywordFound);
+
+            delete second;
             delete head;
+        }
+        TEST_METHOD(testSignUp)
+        {
+            sqlite3* db;
+            int result = sqlite3_open("../data", &db);
+            Assert::AreEqual(SQLITE_OK, result);
+            Authentication auth(db);
+
+            bool tableCreated = auth.createTable();
+            Assert::IsTrue(tableCreated);
+            bool cleanup = auth.executeQuery(std::string("DELETE FROM accounts WHERE username = 'Test_752';"));
+            Assert::IsTrue(cleanup);
+
+            std::string username = "Test_752";
+            std::string password = "test";
+            int userId = -1;
+
+            bool success = auth.signUp(username, password, userId);
+            Assert::IsTrue(success);
+            Assert::IsTrue(userId > 0);
+
+            sqlite3_close(db);
+        }
+        TEST_METHOD(testLogIn)
+        {
+            sqlite3* db;
+            int result = sqlite3_open("../data", &db);
+            Assert::AreEqual(SQLITE_OK, result);
+            Authentication auth(db);
+
+            bool tableCreated = auth.createTable();
+            Assert::IsTrue(tableCreated);
+            bool cleanup = auth.executeQuery(std::string("DELETE FROM accounts WHERE username = 'Test_752';"));
+            Assert::IsTrue(cleanup);
+            std::string username = "Test_752";
+            std::string password = "test";
+            int signUpId = -1;
+
+            bool signUpSuccess = auth.signUp(username, password, signUpId);
+            Assert::IsTrue(signUpSuccess);
+            Assert::IsTrue(signUpId > 0);
+
+            int loginId = -1;
+            bool loginSuccess = auth.logIn(username, password, loginId);
+
+            Assert::IsTrue(loginSuccess);
+            Assert::AreEqual(signUpId, loginId);
+            sqlite3_close(db);
         }
 
 	};
